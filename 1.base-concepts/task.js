8f0885a1f2164f8e25ef5387a03f8226b571e94a
Alex Siver <alexeysiver@gmail.com>
2023-10-18 23:01:37 +0300

    Корректировка задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,16 +1,38 @@
-function getArrayParams(...arr) {
-  if (arr.length === 0) {
-    return {
-      min: undefined,
-      max: undefined,
-      avg: undefined,
-    };
-  }
+"use strict";
 
-  const min = Math.min(...arr);
-  const max = Math.max(...arr);
-  const sum = arr.reduce((acc, curr) => acc + curr, 0);
-  const avg = +(sum / arr.length).toFixed(2);
+function solveEquation(a, b, c) {
+	const arr = [];
+	const discriminant = Math.pow(b, 2) - 4 * a * c;
 
-  return { min, max, avg };
+	if (discriminant === 0) {
+		let root = -b / (2 * a);
+		arr.push(root);
+	} else if (discriminant > 0) {
+		let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push(root1, root2);
+	}
+
+	return arr;
+}
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+	if (
+		isNaN(percent) ||
+		isNaN(contribution) ||
+		isNaN(amount) ||
+		isNaN(countMonths)
+	) {
+		return false;
+	}
+
+	const monthPercent = percent / 100 / 12;
+	const creditBody = amount - contribution;
+	const monthlyPayment =
+		creditBody *
+		(monthPercent +
+			monthPercent / (Math.pow(1 + monthPercent, countMonths) - 1));
+	const fullPayment = Number((monthlyPayment * countMonths).toFixed(2));
+
+	return fullPayment;
 }
