94f9644aa49e879642707a0e80aeb13a9bffdf36
Alex Siver <alexeysiver@gmail.com>
2023-11-23 21:25:28 +0300

    Четвертое задание отредактировано

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -30,3 +30,15 @@ Student.prototype.exclude = function(reason) {
 	delete this.marks;
 	this.excluded = reason;
 };
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба');
+console.log(student2);
