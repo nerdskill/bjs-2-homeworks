613f2070edf0f7e6564852beea28db8ed2109f36
Alex Siver <alexeysiver@gmail.com>
2024-01-31 01:59:38 +0300

    Шестое задание отредактировано (6)

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -17,34 +17,47 @@ function validateCount(value) {
 }
 
 class Triangle {
-	constructor(a, b, c) {
-		if (a + b <= c || a + c <= b || b + c <= a) {
-			throw new Error("Треугольник с такими сторонами не существует");
-		}
+    constructor(a, b, c) {
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
 
-		this.a = a;
-		this.b = b;
-		this.c = c;
-	}
+        this._a = a;
+        this._b = b;
+        this._c = c;
+    }
 
-	get perimeter() {
-		return this.a + this.b + this.c;
-	}
+    get perimeter() {
+        return this._a + this._b + this._c;
+    }
 
-	get area() {
-		const p = this.perimeter / 2;
-		const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
-		return parseFloat(area.toFixed(3));
-	}
+    get area() {
+        const p = this.perimeter / 2;
+        const area = Math.sqrt(p * (p - this._a) * (p - this._b) * (p - this._c));
+        return parseFloat(area.toFixed(3));
+    }
 }
 
 function getTriangle(a, b, c) {
-	try {
-		return new Triangle(a, b, c);
-	} catch (error) {
-		return {
-			area: 'Ошибка! Треугольник не существует',
-			perimeter: 'Ошибка! Треугольник не существует'
-		};
-	}
+    try {
+        const triangle = new Triangle(a, b, c);
+
+        // Используем Object.defineProperty, чтобы сделать свойства только для чтения
+        Object.defineProperty(triangle, 'perimeter', {
+            value: triangle.perimeter,
+            writable: false
+        });
+
+        Object.defineProperty(triangle, 'area', {
+            value: triangle.area,
+            writable: false
+        });
+
+        return triangle;
+    } catch (error) {
+        return {
+            area: 'Ошибка! Треугольник не существует',
+            perimeter: 'Ошибка! Треугольник не существует'
+        };
+    }
 }
